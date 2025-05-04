dc0bff9573de2880a8b1dfd8322583ea62eb5360
Mateo Morinigo <84980263+matemorinigo@users.noreply.github.com>
2025-05-04 13:28:10 -0300

    Implement/init state (#5)
    * Some changes
    
    * remove task timer
    
    * lcd and sd driver
    
    * states removed
    
    * Moving and return to home implemented, logical limit switch modified to use only one
    
    * removed MAX_PRESENCE_SENSORS
    
    * change define
    
    ---------
    
    Co-authored-by: maxamo203 <maxamo.203@gmail.com>

--- Embebido/main.ino
+++ Embebido/main.ino
@@ -2,7 +2,8 @@
 
 void setup()
 {
-  Serial.begin(9600);
+ Serial.begin(9600)
+ initialize()
 }
 
 void loop()
--- Embebido/state_machine_actions.h
+++ Embebido/state_machine_actions.h
@@ -1,107 +1,6 @@
-#pragma once
-#include "timer_schedule.h"
-#include <WiFi.h>
-#include <string.h>
-#include "fisical.h"
 
-const char *ssid = "Wokwi-GUEST";
-const char *password = "";
+#include "setup_utils.h"
 
-const char *ntpServer = "pool.ntp.org";
-const long gmtOffset_sec = -4 * 3600;
-const int daylightOffset_sec = 3600;
-
-TimerHandle_t xTimer = NULL;
-
-void handleTimerCallback(TimerHandle_t xTimer);
-void createNewScheduledTimer();
-/*
-  ---struct tm---
-  Member	Type	Meaning	Range
-  tm_sec	int	seconds after the minute	0-60*
-  tm_min	int	minutes after the hour	0-59
-  tm_hour	int	hours since midnight	0-23
-  tm_mday	int	day of the month	1-31
-  tm_mon	int	months since January	0-11
-  tm_year	int	years since 1900
-  tm_wday	int	days since Sunday	0-6
-  tm_yday	int	days since January 1	0-365
-  tm_isdst	int	Daylight Saving Time flag
-*/
-
-struct tm timeinfo;
-
-void setupWifi()
-{
- Serial.println("Connecting to WiFi...");
- WiFi.begin(ssid, password);
- while (WiFi.status() != WL_CONNECTED)
- {
-  delay(1000);
-  Serial.println("Connecting to WiFi...");
- }
- Serial.println("Connected to WiFi!");
-}
-
-void setupTime()
-{
- configTime(gmtOffset_sec, daylightOffset_sec, ntpServer);
- Serial.println("Time synchronized with NTP server.");
- while (!getLocalTime(&timeinfo, 1000))
- {
-  Serial.println("Failed to obtain time");
-  delay(1000);
- }
- Serial.println("Time succesfully setted");
-}
-
-void printLocalTime()
-{
- Serial.print("Current time: ");
- Serial.print(timeinfo.tm_hour);
- Serial.print(":");
- Serial.print(timeinfo.tm_min);
- Serial.print(":");
- Serial.println(timeinfo.tm_sec);
-}
-
-void handleTimerCallback(TimerHandle_t xTimer)
-{
- if (xTimer != NULL)
- {
-  xQueueSend(timeEventsQueue, &nextPeriod, 0);
-  xTimerDelete(xTimer, 0);
-  xTimer = NULL; // limpiar handle
-  createNewScheduledTimer();
- }
-}
-
-void createNewScheduledTimer()
-{
-
- getLocalTime(&timeinfo, 5); // Update RTC time
-
- searchNextSchedule(&timeinfo); // Set nextPeriod to the next schedule based on the current time
-
- const int timeUntilNextScheduleValue = timeUntilNextSchedule(&timeinfo, &schedule[nextPeriod]); // Calculate time until next schedule
-
- Serial.println(timeUntilNextScheduleValue);
- xTimer = xTimerCreate("ScheduleTime", pdMS_TO_TICKS(timeUntilNextScheduleValue), pdFALSE, NULL, handleTimerCallback);
-
- if (xTimer != NULL)
- {
-  xTimerStart(xTimer, 0);
- }
- else
- {
-  Serial.println("Failed to create timer.");
- }
-}
-
-void detectMovingLimitSwitch(void)
-{
- limitSwitchPassed++;
-}
 
 typedef void (*action)();
 
@@ -109,27 +8,45 @@ typedef void (*action)();
 void initialize()
 {
 
+   int scheduleSetup[MAX_DAYS][MAX_PILLS_PER_DAY] = {
+        {6,14,21}, // Domingo Mañana, Domingo Tarde, Domingo Noche
+        {6, 0, 0},
+        {6, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0},
+        {0, 0, 0}
+    }
+
  // Lee archivo con horarios y dias
  // Calcula el proximo horario y dia
  // Pasa al estado awaiting reminder time
-
  fisicalSetup();
  attachInterrupt(LIMIT_SWITCH_MOVIL, detectMovingLimitSwitch, RISING);
  setupWifi();
  setupTime();
+
+ setupSchedule(scheduleSetup);
+
  createNewScheduledTimer();
  timeEventsQueue = xQueueCreate(MAX_EVENTS_QUEUE, sizeof(events));
+ xTaskCreate(testTimer, "testTimer", 2048, &nextPeriod, 1, &eventTaskHandle);
+
 }
 
 void noScheduleSet();
 void settingSchedule();
 void awaitingTimer();
-void moving();
+void moving() {
+    startMotorRight();
+};
 void scanning();
 void pillDetected();
 void noPillDetected();
 void doseSkipped();
-void returnToHome();
+void returnToHome(){
+    startMotorLeft();
+};
 void error();
 void none();
 
